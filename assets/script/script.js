23da20caa3848fc8a4d631146f03c0db431ce8f2
Domenico <domnquo@gmail.com>
2022-07-02 12:33:47 -0300

    tabela

--- assets/script/script.js
+++ assets/script/script.js
@@ -3,3 +3,20 @@ function menuOnClick() {
     document.getElementById("nav").classList.toggle("change");
     document.getElementById("menu-bg").classList.toggle("change-bg");
   }
+
+
+  function hydrate(string) {
+    let count = 0
+    let numbers = string.replace(/[^0-9]/g,'');
+    console.log(numbers);
+    for(let i = 0; i < numbers.length; i++){
+     count += parseInt(numbers[i])
+    }
+    if(count > 1){
+      return count + ' copos de água'
+    } else {
+      return count + ' copo de água'
+    } 
+  }
+
+  console.log(hydrate('1 cachaça'));
